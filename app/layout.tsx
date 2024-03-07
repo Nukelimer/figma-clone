d9b1702cee83afcb701f8ba1e62b2dd0703fa354
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-03-07 11:25:52 +0100

    made it only desktop view only.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,24 +3,21 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Room } from "./Room";
 
+
 const roboto = Roboto({
   subsets: ["latin"],
   variable: "--global-font-family",
   weight: ["100", "300", "400", "500", "700", "900"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "Figma Clone",
   description:
     "Trying my best to be a world class. by buiding with fabric.js and Liveblock",
-    icons: {
-      icon: '/icon.ico', // /public path
-    },
+  icons: {
+    icon: "/icon.ico", // /public path
+  },
 };
-
-
 
 export default function RootLayout({
   children,
@@ -29,9 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
       <body className={`${roboto.className} bg-slate-800 text-white`}>
-        <Room>{children}</Room>
+        <div className="lg:hidden flex w-full h-screen justify-center items-center flex-col my-auto">
+         
+          <p className="self-center justify-center">Heyy There! I am sorry this application can run only optimally on desktop screens. please switch to a laptop or tablet device.🤓</p>
+        </div>
+        <div className="hidden lg:block">
+          <Room>{children}</Room>
+        </div>
       </body>
     </html>
   );
